cc45f38b9303a0605b6e459e8ea93d2f2ce94b8f
srinaaths <srinaath.s@surveysparrow.com>
2022-07-21 19:36:51 +0530

    dev in progress

--- src/MovieEle.js
+++ src/MovieEle.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import './movieEle.css'
+import MovieRating from './MovieRating'
 
 const MovieEle = ({ movie }) => {
   const [enabled, setEnabled] = useState(false)
   const clickFunction = (e) => {
     console.log('clicked');
+    setEnabled(true)
   }
   return (
     <div>
       <button className='movie' onClick={e => clickFunction(e)}>{movie.name}</button>
+      {/* {enabled && (<div><input type="text" placeholder='review'></input></div>)} */}
+      {enabled && (<MovieRating movie={movie} />)}
     </div>
   )
 }
---
+++ src/MovieRating.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+import React, { useState } from 'react'
+const MovieRating = ({movie}) => {
+    const [movies, setMovies] = useState([])
+    const [rating, setRating] = useState(0)
+    const [review, setReview] = useState('')
+    const [movieId, setMovieId] = useState(0)
+    const [currentPage, setCurrentPage] = useState(1)
+    const [moviesPerPage, setMoviessPerPage] = useState(10)
+    const submitRating = async() => {
+        const data = {
+          rating: rating,
+          review: review,
+          movieId: movieId,
+          userId: 1
+        }
+        console.log('data is ' + data);
+      const res = await axios.post('http://localhost:8080/addrating', data)
+      console.log(res);
+    }   
+    return (
+        <div>
+            <label htmlFor="">Rating</label>
+            <input type="number" onChange={(e) => {
+                setRating(e.target.value)
+                setMovieId(movie.id)
+            }} />
+            <br />
+            <label htmlFor="">Review</label>
+            <input type="text" onChange={(e) => {
+                setReview(e.target.value)
+                setMovieId(movie.id)
+            }
+            } />
+            <br />
+            <input type="submit" onClick={submitRating} />
+        </div>
+    )
+}
+
+export default MovieRating 

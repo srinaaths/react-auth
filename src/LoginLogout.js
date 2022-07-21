2d7e38aaf02abc3e82847d451fcfedfad503884b
srinaaths <srinaath.s@surveysparrow.com>
2022-07-21 15:19:53 +0530

    redirection after login to movieslist set up yet to implement it in the fresh page

--- src/LoginLogout.js
+++ src/LoginLogout.js
@@ -1,91 +1,92 @@
 import { useState } from "react"
 import axios from 'axios'
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom"
 import Auth from "./Auth"
 import App from "./App"
 import AuthPage from "./AuthPage"
 import store from "./redux-components/store"
+import MoviesRatingPage from "./MoviesRatingPage"
 
 const LoginLogout = () => {
 
-	const [usernameReg, setUsernameReg] = useState('')
-	const [username, setUsername] = useState('')
-	const [passwordReg, setPasswordReg] = useState('')
-	const [password, setPassword] = useState('')
-	const [loginStatus, setLoginStatus] = useState(false)
-	const [isAuthenticated, setIsAuthenticated] = useState(false)
+    const [usernameReg, setUsernameReg] = useState('')
+    const [username, setUsername] = useState('')
+    const [passwordReg, setPasswordReg] = useState('')
+    const [password, setPassword] = useState('')
+    const [loginStatus, setLoginStatus] = useState(false)
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
 
-	let userIdentity = null;
+    let userIdentity = null;
 
-	const usernameRegUpdate = (event) => {
-		setUsernameReg(event.target.value)
-	}
-	const passwordRegUpdate = (event) => {
-		setPasswordReg(event.target.value)
-	}
-	const usernameUpdate = (event) => {
-		setUsername(event.target.value)
-	}
-	const passwordUpdate = (event) => {
-		setPassword(event.target.value)
-	}
-	const submitFunction = async (e) => {
-		e.preventDefault();
-		console.log('called');
-		const user = {
-			name: usernameReg,
-			password: passwordReg
-		}
-		try {
-			const res = await axios.post('http://localhost:8080/adduser', user);
-			console.log(res);
-		} catch (error) {
-			console.log(error);
-		}
-	}
-	const userCheck = async (e) => {
-		e.preventDefault();
-		const user = {
-			name: username,
-			password: password
-		}
-		try {
-			const res = await axios.post('http://localhost:8080/loginuser', user)
-			console.log(res);
-			console.log(res.data.auth)
-			if (!res.data.auth) {
-				setLoginStatus(false)
-                setIsAuthenticated(false)
-                store.dispatch({type: 'USER_LOGOUT'})
-            }
-			else {
-				setLoginStatus(true)
-				localStorage.setItem('token', res.data.token)
-                userAuthenticated();
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
-	const logoutFunction = async () => {
+    const usernameRegUpdate = (event) => {
+        setUsernameReg(event.target.value)
+    }
+    const passwordRegUpdate = (event) => {
+        setPasswordReg(event.target.value)
+    }
+    const usernameUpdate = (event) => {
+        setUsername(event.target.value)
+    }
+    const passwordUpdate = (event) => {
+        setPassword(event.target.value)
+    }
+    const submitFunction = async (e) => {
+        e.preventDefault();
         console.log('called');
-        store.dispatch({type: 'USER_LOGOUT'})
+        const user = {
+            name: usernameReg,
+            password: passwordReg
+        }
+        try {
+            const res = await axios.post('http://localhost:8080/adduser', user);
+            console.log(res);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const userCheck = async (e) => {
+        e.preventDefault();
+        const user = {
+            name: username,
+            password: password
+        }
+        try {
+            const res = await axios.post('http://localhost:8080/loginuser', user)
+            console.log(res);
+            console.log(res.data.auth)
+            if (!res.data.auth) {
+                setLoginStatus(false)
+                setIsAuthenticated(false)
+                store.dispatch({ type: 'USER_LOGOUT' })
+            }
+            else {
+                setLoginStatus(true)
+                localStorage.setItem('token', res.data.token)
+                userAuthenticated();
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    const logoutFunction = async () => {
+        console.log('called');
+        store.dispatch({ type: 'USER_LOGOUT' })
         console.log(store.getState());
         setIsAuthenticated(false)
     }
-	const userAuthenticated = async () => {
-		const res = await axios.get('http://localhost:8080/isuserauth'
-			,
-			{
-				headers: {
-					'x-access-token': localStorage.getItem('token'),
-				}
-			}
-		)
-		console.log(res);
-		if (res.data.isAuthenticated) {
-			userIdentity = res.data.userId
-			setIsAuthenticated(true)
+    const userAuthenticated = async () => {
+        const res = await axios.get('http://localhost:8080/isuserauth'
+            ,
+            {
+                headers: {
+                    'x-access-token': localStorage.getItem('token'),
+                }
+            }
+        )
+        console.log(res);
+        if (res.data.isAuthenticated) {
+            userIdentity = res.data.userId
+            setIsAuthenticated(true)
             store.dispatch({
                 type: 'USER_LOGIN',
                 payload: {
@@ -95,37 +96,41 @@ const LoginLogout = () => {
                 }
             })
             console.log(store.getState());
-		}
+        }
         else {
             console.log('auth set to false');
             setIsAuthenticated(false)
-            store.dispatch({type: 'USER_LOGOUT'})
+            store.dispatch({ type: 'USER_LOGOUT' })
         }
-	}
-	return (
-		<div>
-			<h2> Registration</h2>
-			<form action="" onSubmit={(e) => submitFunction(e)}>
-				<label htmlFor="">Name</label>
-				<input type="text" onChange={usernameRegUpdate} /> <br /> <br />
-				<label htmlFor="">Password</label>
-				<input type="password" onChange={passwordRegUpdate} /> <br /> <br />
-				<input type="submit" value="Register"/>
-			</form>
-			<h2> Login</h2>
-			<form action="" onSubmit={(e) => userCheck(e)}>
-				<label htmlFor="">Name</label>
-				<input type="text" onChange={usernameUpdate} /> <br /> <br />
-				<label htmlFor="">Password</label>
-				<input type="password" onChange={passwordUpdate} /> <br /> <br />
-				<input type="submit" value="Login" />
-			</form>
+    }
+    return (
+        <div>
+            <h2> Registration</h2>
+            <form action="" onSubmit={(e) => submitFunction(e)}>
+                <label htmlFor="">Name</label>
+                <input type="text" onChange={usernameRegUpdate} /> <br /> <br />
+                <label htmlFor="">Password</label>
+                <input type="password" onChange={passwordRegUpdate} /> <br /> <br />
+                <input type="submit" value="Register" />
+            </form>
+            <h2> Login</h2>
+            <form action="" onSubmit={(e) => userCheck(e)}>
+                <label htmlFor="">Name</label>
+                <input type="text" onChange={usernameUpdate} /> <br /> <br />
+                <label htmlFor="">Password</label>
+                <input type="password" onChange={passwordUpdate} /> <br /> <br />
+                <input type="submit" value="Login" />
+            </form>
             {console.log(isAuthenticated)}
             {console.log(store.getState())}
+            <Router>
+                <Route path='/movieslist' component={MoviesRatingPage}></Route>
+                {isAuthenticated && <Redirect to='/movieslist' />}
+            </Router>
             <div>{isAuthenticated && <AuthPage isAuthenticated />}</div>
             <div>{isAuthenticated && <button onClick={logoutFunction}>Logout</button>}</div>
-		</div>
-	)
+        </div>
+    )
 }
 
 export default LoginLogout
---
+++ src/MoviesRatingPage.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { Redirect } from 'react-router-dom'
+import store from './redux-components/store'
+
+const MoviesRatingPage = () => {
+  const [isAuth, setIsAuth] = useState((store.getState() && store.getState().authStatus))
+  if(!isAuth)
+    return <Redirect to='/' />
+  return (
+    <div>MoviesRatingPage</div>
+  )
+}
+
+export default MoviesRatingPage
